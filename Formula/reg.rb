93e16649038b1ab054e1fd1fc4d3afe75fc8ae66
ciiiii <allen.ccccnm@gmail.com>
2021-12-14 13:00:44 +0800

    fix reg formula

--- Formula/reg.rb
+++ Formula/reg.rb
@@ -3,16 +3,18 @@ require "language/go"
 class Reg < Formula
     desc " Docker registry v2 command line client and repo listing generator with security checks. "
     homepage "https://github.com/genuinetools/reg"
-    head "https://github.com/genuinetools/reg", branch: "master"
+    # head "https://github.com/genuinetools/reg", branch: "master"
+    url "https://github.com/genuinetools/reg/archive/v0.16.1.tar.gz"
+    sha256 "b65787bff71bff21f21adc933799e70aa9b868d19b1e64f8fd24ebdc19058430"    
 
     depends_on "go" => :build
   
     def install
       ENV["GOPATH"] = buildpath
-      Dir.chdir buildpath/"src/github.com/genuinetools/reg" do
-        system "go", "get", "./"
-      end
-      system "go", "build", "-o", "#{bin}/reg"
+      (buildpath/"build/src").mkpath
+      ln_s buildpath, buildpath/"build/src/github.com"
+      system "GOPATH=$PWD/build go get github.com/genuinetools/reg"
+      bin.install "build/bin/ls-go"
     end
   
     test do
