5c48452f3cd3e9c5d90ced8f2d60ad25a929312d
ciiiii <allen.ccccnm@gmail.com>
2021-12-14 11:32:02 +0800

    add reg formula

---
+++ Formula/reg.rb
@@ -0,0 +1,15 @@
+class Reg < Formula
+    desc " Docker registry v2 command line client and repo listing generator with security checks. "
+    homepage "https://github.com/genuinetools/reg"
+    head "https://github.com/genuinetools/reg", branch: "master"
+    
+    depends_on "go" => :build
+  
+    def install
+      system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "./main.go"
+    end
+  
+    test do
+      assert_match version.to_s, shell_output("#{bin}/reg version 2>&1")
+    end
+  end
